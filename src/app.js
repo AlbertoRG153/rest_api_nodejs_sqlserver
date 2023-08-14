6f495accd2686d22674278a11a43ab3b652c032f
Alberto Romero <alberto.romerog27@gmail.com>
2023-08-13 21:08:19 -0600

    ajuste

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import config from './config'
-import empresasRoutes from './routes/empresas.routes'
-import agentesRoutes from './routes/agentes.routes'
-import empleadosRoutes from './routes/empleados.routes'
+// import empresasRoutes from './routes/empresas.routes'
+// import agentesRoutes from './routes/agentes.routes'
+// import empleadosRoutes from './routes/empleados.routes'
 
 const app = express();
 
@@ -13,6 +13,6 @@ app.set('port', config.port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(empresasRoutes, agentesRoutes, empleadosRoutes);
+// app.use(empresasRoutes, agentesRoutes, empleadosRoutes);
 
 export default app
